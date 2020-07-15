d6aca33a2174bb6800b4761ee9e0f6b407947ad2
Dmitry-Bespalov <mailto:bespalov0928@mail.ru>
2020-07-15 23:42:12 +0300

    2/2.1/3/2. Создать контейнер на базе связанного списка [#314207]

---
+++ src/main/java/ru/job4j/chapter001/list/SimpleLinked.java
@@ -0,0 +1,83 @@
+package ru.job4j.chapter001.list;
+
+import java.util.*;
+
+public class SimpleLinked<E> implements Iterable<E> {
+
+    transient int size = 0;
+    transient Node<E> first;
+    transient Node<E> last;
+    private int modCount = 0;
+
+    private static class Node<E> {
+        E item;
+        Node<E> next;
+        Node<E> prev;
+
+        Node(Node<E> prev, E element, Node<E> next) {
+            this.item = element;
+            this.next = next;
+            this.prev = prev;
+        }
+    }
+
+    public void add(E value) {
+        if (first == null) {
+            Node newNode = new Node(null, value, null);
+            first = newNode;
+            last = newNode;
+        } else {
+            Node newNode = new Node(last, value, null);
+            last.next = newNode;
+            //newNode.prev = last;
+            last = newNode;
+        }
+        size++;
+        modCount++;
+    }
+
+    public E get(int index) {
+        Objects.checkIndex(index, size);
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+        Node rsl = first;
+        for (int i = 0; i < index; i++) {
+            rsl = rsl.next;
+        }
+        return (E) rsl.item;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+
+        Iterator<E> it = new Iterator<E>() {
+            int point = 0;
+            int expectedModCount = modCount;
+
+            @Override
+            public boolean hasNext() {
+                return point < size;
+            }
+
+            @Override
+            public E next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                if (expectedModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                Node rsl = first;
+
+                if (point != 0) {
+                    rsl = first.next;
+                }
+                point++;
+                return (E) rsl.item;
+            }
+        };
+
+        return it;
+    }
+}
---
+++ src/test/java/ru/job4j/chapter001/list/SimpleLinkedTest.java
@@ -0,0 +1,40 @@
+package ru.job4j.chapter001.list;
+
+import org.junit.Test;
+
+import javax.swing.text.html.HTMLDocument;
+
+import java.util.Iterator;
+
+import static org.junit.Assert.*;
+
+public class SimpleLinkedTest {
+
+    @Test
+    public void add() {
+        SimpleLinked<Integer> simpleLinked = new SimpleLinked<>();
+        simpleLinked.add(1);
+        simpleLinked.add(2);
+        simpleLinked.add(3);
+    }
+
+    @Test
+    public void get() {
+        SimpleLinked<Integer> simpleLinked = new SimpleLinked<>();
+        simpleLinked.add(1);
+        simpleLinked.add(2);
+        simpleLinked.add(3);
+        simpleLinked.get(2);
+    }
+
+    @Test
+    public void iterat() {
+        SimpleLinked<Integer> simpleLinked = new SimpleLinked<>();
+        simpleLinked.add(1);
+        simpleLinked.add(2);
+        simpleLinked.add(3);
+        Iterator it = simpleLinked.iterator();
+        it.next();
+        it.next();
+    }
+}
