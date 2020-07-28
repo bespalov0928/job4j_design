f22fd8c9403cadea752e4eaf2503a6cb11d648a3
Dmitry-Bespalov <mailto:bespalov0928@mail.ru>
2020-07-28 07:40:30 +0300

    2/2.1/5/2. Без переопределения equals и hashCode [#314238]

---
+++ src/main/java/ru/job4j/chapter001/map/MapNotEquals.java
@@ -0,0 +1,19 @@
+package ru.job4j.chapter001.map;
+
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapNotEquals {
+
+    public static void main(String[] args) {
+        User first = new User("Ivan", 1, new GregorianCalendar(1530, 8, 25));
+        User second = new User("Napoleon", 1, new GregorianCalendar(1804, 5, 18));
+
+        Map<User, Object> map = new HashMap<>();
+        map.put(first, "first");
+        map.put(second, "second");
+        System.out.println(map);
+    }
+}
+
