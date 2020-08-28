29ad2a39527180b8ab14537c7be47e015216cd62
Dmitry-Bespalov <mailto:bespalov0928@mail.ru>
2020-08-28 21:09:16 +0300

    2/2.1/7/1. Экзамен. Структуры данных и алгоритмы. [#314201]

--- src/main/java/ru/job4j/chapter001/FreezeStr.java
+++ src/main/java/ru/job4j/chapter001/FreezeStr.java
@@ -7,23 +7,26 @@ import java.util.*;
  */
 public class FreezeStr {
     public static boolean eq(String left, String right) {
-        Map<Character, Integer> map = new HashMap<>();
-        char[] arrLeft = left.toCharArray();
-        char[] arrRight = right.toCharArray();
-        for (Character chL : arrLeft) {
-            Integer value = map.get(chL);
-            value = value == null ? 1 : ++value;
-            map.put(chL, value);
+        if (left.length() != right.length()) {
+            return false;
         }
-        for (Character chR : arrRight) {
-            Integer value = map.get(chR);
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < left.length(); i++) {
+            char key = left.charAt(i);
+            Integer value = map.get(key);
+            value = value == null ? 1 : ++value;
+            map.put(left.charAt(i), value);
+        }
+        for (int i = 0; i < right.length(); i++) {
+            char key = right.charAt(i);
+            Integer value = map.get(key);
             if (value == null) {
-                continue;
+                return false;
             }
             if (value == 1) {
-                map.remove(chR);
+                map.remove(right.charAt(i));
             } else {
-                map.put(chR, --value);
+                map.put(right.charAt(i), --value);
             }
         }
         return map.isEmpty();
