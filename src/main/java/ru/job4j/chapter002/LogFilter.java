5d4671a28065011d1ee473c0a93859cdf41fa38c
Dmitry-Bespalov <mailto:bespalov0928@mail.ru>
2020-08-30 07:49:59 +0300

    2/2.2/1/0.4. BufferedOutputStream [#314256]

--- src/main/java/ru/job4j/chapter002/LogFilter.java
+++ src/main/java/ru/job4j/chapter002/LogFilter.java
@@ -12,11 +12,11 @@ public class LogFilter {
             String line;
             while ((line = in.readLine()) != null) {
                 String[] arr = line.split("/");
-                String arrLastString =  arr[arr.length - 1];
+                String arrLastString = arr[arr.length - 1];
                 String tmp = String.format("%s%s%s", arrLastString.charAt(5), arrLastString.charAt(6), arrLastString.charAt(7));
-                if (tmp .equals("404")) {
+                if (tmp.equals("404")) {
                     //continue;
-                    lines.add(line);
+                    lines.add(line + System.lineSeparator());
                 }
                 //System.out.println(line);
                 //lines.add(line);
@@ -30,8 +30,19 @@ public class LogFilter {
         return lines;
     }
 
+    public static void save(List<String> log, String file) {
+        try (PrintWriter out = new PrintWriter(new BufferedOutputStream(new FileOutputStream(file)))) {
+            for (String str: log) {
+                out.write(str);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) {
         List<String> log = filter("log.txt");
         System.out.println(log);
+        save(log, "log_write.txt");
     }
 }
