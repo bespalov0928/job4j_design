0a4fa0b123c4f64290701b716578e6167536e5c6
Dmitry-Bespalov <mailto:bespalov0928@mail.ru>
2020-09-12 09:03:55 +0300

    2/2.2/2/1. Бот [#314270]

--- src/main/java/ru/job4j/chapter002/Io/Socket/EchoServer.java
+++ src/main/java/ru/job4j/chapter002/Io/Socket/EchoServer.java
@@ -1,29 +1,50 @@
 package ru.job4j.chapter002.Io.Socket;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketException;
 
 public class EchoServer {
     public static void main(String[] args) throws IOException {
-        try (ServerSocket server = new ServerSocket(9002)) {
+        try (ServerSocket server = new ServerSocket(9003)) {
             while (!server.isClosed()) {
                 Socket socket = server.accept();
-                try (OutputStream out = socket.getOutputStream();
-                     BufferedReader in = new BufferedReader(
-                             new InputStreamReader(socket.getInputStream()))) {
+                //try (OutputStream out = socket.getOutputStream();
+                try (BufferedWriter out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+                    BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
                     String str;
                     while (!(str = in.readLine()).isEmpty()) {
                         System.out.println(str);
-                        if (str.contains("bye")) {
+                        //System.out.println(str.getClass());
+                        if (str.contains("Bye")) {
+                            System.out.println("Bye");
                             server.close();
+                            break;
+                        } else if (str.contains("Exit")) {
+                            System.out.println("Exit");
+                            server.close();
+                            break;
+                        } else if (str.contains("Hello")) {
+                            System.out.println("Hello");
+                            out.write("Hello.");
+                            out.flush();
+                            break;
+                        } else if (str.contains("What")) {
+                            System.out.println("What");
+                            out.write("What.");
+                            out.flush();
+                            break;
+                        } else {
+                            System.out.println("Other");
+                            out.write(str);
+                            out.flush();
+                            break;
                         }
                     }
-                    out.write("HTTP/1.1 200 OK\r\n\\".getBytes());
+                    //out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                    //out.write("Hello, dear friend.".getBytes());
+                    //out.flush();
                 }
             }
         }
