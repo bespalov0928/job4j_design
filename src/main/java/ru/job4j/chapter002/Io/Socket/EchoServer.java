b57883e1086e2362dabe83e95fa0448451182235
Dmitry-Bespalov <mailto:bespalov0928@mail.ru>
2020-09-13 19:14:10 +0300

    2/2.2/2/1. Бот [#314270]

--- src/main/java/ru/job4j/chapter002/Io/Socket/EchoServer.java
+++ src/main/java/ru/job4j/chapter002/Io/Socket/EchoServer.java
@@ -10,88 +10,89 @@ import java.util.function.Consumer;
 public class EchoServer {
     public static void main(String[] args) throws IOException {
         Map<String, Consumer<String>> map = new HashMap<>();
-        //String request = "";
         try (ServerSocket server = new ServerSocket(9004)) {
-//            System.out.println("0" + server.isClosed());
             while (!server.isClosed()) {
                 Socket socket = server.accept();
-//                System.out.println("1" + socket.isClosed());
-                //try (OutputStream out = socket.getOutputStream();
                 try (BufferedWriter out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
                      BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
-                    //map = getMap(server, out);
-//                    System.out.println("2" + socket.isClosed());
-                    String str = "123";
+                    map = getMap(server, out);
+                    String str;
                     while (!(str = in.readLine()).isEmpty()) {
-//                        System.out.println("3" + socket.isClosed());
-//                        System.out.println(str);
-//                        String[] arrRequest = str.split("=");
-//                        for (String s:arrRequest) {
+                        System.out.println(str);
+                        String[] arrFirst = str.split(" ");
+//                        for (String s:arrFirst) {
 //                            System.out.println(s);
 //                        }
-//                        System.out.println("4" + socket.isClosed());
-//                        String strTmp = arrRequest[1];
-//                        Consumer predicate = map.get(strTmp);
-//                        if (predicate == null) {
-//                            System.out.println("5" + socket.isClosed());
-                            System.out.println(str.substring(5));
-                            out.write("str:" + str.substring(5) + "\r\n\r\n");
+                        String strTmp = arrFirst[1];
+                        String[] arrSecond = strTmp.split("=");
+                        for (String s_ : arrSecond) {
+                            System.out.println(s_);
+                        }
+
+                        //System.out.println(arrSecond[1]);
+                        Consumer predicate = map.get(arrSecond[1]);
+                        if (predicate == null) {
+                            out.write("HTTP/1.1 200 OK\r\n\r\n");
+                            out.write(str + "\r\n\r\n");
                             out.flush();
                             break;
-//                        }
-                        //predicate.accept(strTmp);
-                        //break;
+                        }
+                        predicate.accept(arrSecond[1]);
+                        break;
                     }
                     //out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
                     //out.write("Hello, dear friend.".getBytes());
-//                    out.flush();
-               }
+                    //out.flush();
+                }
             }
         }
     }
 
     public static Map<String, Consumer<String>> getMap(ServerSocket server, BufferedWriter out) {
         Map<String, Consumer<String>> map = new HashMap<>();
-//        map.put("Bye", (String p) -> {
-//            try {
-//                System.out.println(p + System.lineSeparator());
-//                server.close();
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//            }
-//        });
-//        map.put("Exit", (String p) -> {
-//            try {
-//                System.out.println(p + System.lineSeparator());
-//                server.close();
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//            }
-//        });
-//        map.put("Hello", (p) -> {
-//            try {
-//                System.out.println(p + System.lineSeparator());
-//                out.write(String.format("%s\n", p));
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//            }
-//        });
-//        map.put("What", (p) -> {
-//            try {
-//                System.out.println(p + System.lineSeparator());
-//                out.write(String.format("%s\n", p));
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//            }
-//        });
-//        map.put("Any", (String p) -> {
-//            try {
-//                System.out.println(p + System.lineSeparator());
-//                out.write(String.format("%s\n", p));
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//            }
-//        });
+        map.put("Bye", (String p) -> {
+            try {
+                System.out.println("Bye:" + p + System.lineSeparator());
+                server.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+        map.put("Exit", (String p) -> {
+            try {
+                System.out.println("Exit:" + p + System.lineSeparator());
+                server.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+        map.put("Hello", (p) -> {
+            try {
+                System.out.println("Hello:" + p + System.lineSeparator());
+                out.write("HTTP/1.1 200 OK\r\n\r\n");
+                out.write(String.format("%s\n", p));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+        map.put("What", (p) -> {
+            try {
+                System.out.println("What:" + p + System.lineSeparator());
+                out.write("HTTP/1.1 200 OK\r\n\r\n");
+                out.write(String.format("%s\n", p));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+        map.put("Any", (String p) -> {
+            try {
+                System.out.println("Any:" + p + System.lineSeparator());
+                out.write("HTTP/1.1 200 OK\r\n\r\n");
+                out.write(String.format("%s\n", p));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
         return map;
     }
 }
