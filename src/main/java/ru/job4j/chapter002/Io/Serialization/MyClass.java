e36f1decbdcf321f2296f409b30449cd86e2e4b5
Dmitry-Bespalov <mailto:bespalov0928@mail.ru>
2020-09-15 07:52:11 +0300

    2/2.2/4/1. Что такое Сериализация? [#314272]

---
+++ src/main/java/ru/job4j/chapter002/Io/Serialization/MyClass.java
@@ -0,0 +1,24 @@
+package ru.job4j.chapter002.Io.Serialization;
+
+import java.io.Serializable;
+
+public class MyClass implements Serializable {
+    String s;
+    int i;
+    double d;
+
+    public MyClass(String s, int i, double d) {
+        this.s = s;
+        this.i = i;
+        this.d = d;
+    }
+
+    @Override
+    public String toString() {
+        return "MyClass{" +
+                "s='" + s + '\'' +
+                ", i=" + i +
+                ", d=" + d +
+                '}';
+    }
+}
