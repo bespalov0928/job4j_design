ffc9b84435006c9aad8f12eaef1a4eadcbc2e080
Dmitry-Bespalov <mailto:bespalov0928@mail.ru>
2020-10-11 01:24:32 +0700

    2/2.3/5/0. Видео [#314166]

---
+++ src/main/java/ru/job4j/chapter003/ConnectionDemo.java
@@ -0,0 +1,54 @@
+package ru.job4j.chapter003;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.lang.module.FindException;
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ConnectionDemo {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        Class.forName("org.postgresql.Driver");
+        String url = "jdbc:postgresql://localhost:5432/idea_db";
+        Map<String, String> map = readFile();
+        String login = map.get("username");
+        String password = map.get("password");
+        try (Connection connection = DriverManager.getConnection(url, login, password)) {
+            DatabaseMetaData metaData = connection.getMetaData();
+            System.out.println(metaData.getUserName());
+            System.out.println(metaData.getURL());
+        }
+    }
+
+    private static Map<String, String> readFile() {
+        Map rsl = new HashMap();
+        try (BufferedReader in = new BufferedReader(new FileReader("app.properties"))) {
+            String line;
+            while ((line = in.readLine()) != null) {
+                String[] arr = line.split("=");
+                if (arr.length != 2) {
+                    continue;
+                }
+                String key = arr[0];
+                if (key.lastIndexOf("password") > 0) {
+                    rsl.put("password", arr[1]);
+                }
+                if (key.lastIndexOf("username") > 0) {
+                    rsl.put("username", arr[1]);
+                }
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return rsl;
+    }
+}
