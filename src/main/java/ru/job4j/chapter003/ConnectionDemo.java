81fb2aad1c7ec5a716c3e0148053929a8c8f31dd
Dmitry-Bespalov <mailto:bespalov0928@mail.ru>
2020-10-11 19:32:07 +0700

    2/2.3/5/0. Видео [#314166]

--- src/main/java/ru/job4j/chapter003/ConnectionDemo.java
+++ src/main/java/ru/job4j/chapter003/ConnectionDemo.java
@@ -31,17 +31,31 @@ public class ConnectionDemo {
         try (BufferedReader in = new BufferedReader(new FileReader("app.properties"))) {
             String line;
             while ((line = in.readLine()) != null) {
-                String[] arr = line.split("=");
-                if (arr.length != 2) {
-                    continue;
+
+                Integer indexPas = line.lastIndexOf("password");
+                Integer indexName = line.lastIndexOf("username");
+                if (indexPas > 0) {
+                    rsl.put("password", line.substring(line.indexOf("=")));
+                } else if (indexName > 0) {
+                    rsl.put("username", line.substring(line.indexOf("=")));
                 }
-                String key = arr[0];
-                if (key.lastIndexOf("password") > 0) {
-                    rsl.put("password", arr[1]);
-                }
-                if (key.lastIndexOf("username") > 0) {
-                    rsl.put("username", arr[1]);
-                }
+
+//                if (line.lastIndexOf("password") < 0 || line.lastIndexOf("username") < 0) {
+//                    continue;
+//                }
+//
+//                String key = line.substring(pasIndex);
+//                String[] arr = line.split("=");
+//                if (arr.length != 2) {
+//                    continue;
+//                }
+//                key = arr[0];
+//                if (key.lastIndexOf("password") > 0) {
+//                    rsl.put("password", arr[1]);
+//                }
+//                if (key.lastIndexOf("username") > 0) {
+//                    rsl.put("username", arr[1]);
+//                }
             }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
