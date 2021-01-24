b0aa612bd020e90f54ed299d056fb1f21a368437
Dmitry-Bespalov <mailto:bespalov0928@mail.ru>
2021-01-24 09:57:51 +0300

    3/1/4/0. Управление нитью через wait. [#444788]

---
+++ src/main/java/ru/job4j/chapter007/MultiUser.java
@@ -0,0 +1,25 @@
+package ru.job4j.chapter007;
+
+public class MultiUser {
+    public static void main(String[] args) {
+        Barrier barrier = new Barrier();
+        Thread master = new Thread(
+                ()->{
+                    System.out.println(Thread.currentThread().getName() + " started");
+                    barrier.on();
+                },
+                "Master"
+        );
+
+        Thread slave = new Thread(
+                ()->{
+                    barrier.check();
+                    System.out.println(Thread.currentThread().getName() + " started");
+                },
+                "Slave"
+        );
+
+        slave.start();
+        master.start();
+    }
+}
